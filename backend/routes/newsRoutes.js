cb30af185ca2b758d40f1fa19ab34003a2cf7309
tu35pha <tu35pha@gmail.com>
2025-04-03 13:40:15 +0700

    remove file backend old

--- backend/routes/newsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getNews, createNews } = require("../controllers/newsController");
-const router = express.Router();
-
-router.get("/", getNews);
-router.post("/", createNews);
-
-module.exports = router;
--- backend/routes/usersRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getUsers, createUser, deleteUser } = require("../controllers/usersController");
-const router = express.Router();
-
-router.get("/", getUsers);
-router.post("/", createUser);
-router.delete("/:id", deleteUser);
-
-module.exports = router;
