6317037ad16b98e810e7e3abbbc2050043597fcf
Huỳnh Phạm Long Triều <trieu7a2005@gmail.com>
2025-05-25 09:00:16 +0700

    update_4

--- be_v1/api/v1/routes/document.route.js
+++ be_v1/api/v1/routes/document.route.js
@@ -2,11 +2,7 @@ const express = require("express");
 const controllers = require("../controllers/document.controller");
 const verifyToken = require("../../../validate/verifyToken");
 const router = express.Router();
-
-// Dùng uploadMiddleware từ controller (đã cấu hình memoryStorage)
 router.post("/upload", verifyToken, controllers.uploadMiddleware, controllers.uploadFile);
-
-// Các route khác
 router.get("", verifyToken, controllers.listDocs);
 router.get("/detail/:id", verifyToken, controllers.detailDoc);
 router.patch("/update/:id", verifyToken, controllers.editDoc);
@@ -14,5 +10,4 @@ router.delete("/delete/:id", verifyToken, controllers.deleteDoc);
 router.get("/admin/:check", verifyToken, controllers.filterDocuments);
 router.get("/getByIdUser/:idUser", verifyToken, controllers.getDocByIdUser);
 router.get("/find", verifyToken, controllers.findDoc);
-
 module.exports = router;
