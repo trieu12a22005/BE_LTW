c1480324dad66f7cadd4800b17a3809d8a1fb7e0
Huỳnh Phạm Long Triều <trieu7a2005@gmail.com>
2025-05-25 22:24:22 +0700

    fix moongoo 1

--- be_v1/config/database.js
+++ be_v1/config/database.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose")
 
-module.exports.connect = async() =>{
-    try{
+module.exports.connect = async () => {
+    try {
+        console.log("====MONGO_URL====", process.env.MONGO_URL); // <-- Thêm dòng này!
         await mongoose.connect(process.env.MONGO_URL);
-        console.log(process.env.MONGO_URL)
-        console.log("Connect Success")
-
-    } catch(error) {
-        console.log("Connect Error")
+        console.log("Connect Success");
+    } catch (error) {
+        console.log("Connect Error", error);
     }
 }
